0d4ce5845ab2a0946434ccd3cdc18f772b79fa00
endtry <lucas123phan@gmail.com>
2019-02-25 17:31:46 -0800

    add main header, footer and content component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'LabAssignment3';
+  
+  constructor() {
+
+  }
+
+
 }
